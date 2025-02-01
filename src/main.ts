6d00f1ab68f614584858530e99472cd941671c00
ali <terminalexe1@gmail.com>
2025-02-01 04:57:26 +0200

    Added Fastify and SWC configuration

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      logger: {
+        level: 'info',
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+            translateTime: 'HH:MM:ss Z',
+            ignore: 'pid,hostname',
+          },
+        },
+      },
+    }),
+  );
+
+  // Optional: Enable CORS
+  await app.register(import('@fastify/cors'), {
+    origin: true,
+  });
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+}
+
+void bootstrap();
