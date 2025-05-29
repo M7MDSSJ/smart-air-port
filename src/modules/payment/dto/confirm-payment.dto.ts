121da9e3cb7520d68dc47e8a371419b8aaf7ce61
Ali Mohammed <terminalexe1@gmail.com>
2025-05-29 05:34:03 +0300

    updates

--- src/modules/payment/dto/confirm-payment.dto.ts
+++ src/modules/payment/dto/confirm-payment.dto.ts
@@ -1,4 +1,4 @@
-import { IsString } from 'class-validator';
+import { IsString, IsOptional } from 'class-validator';
 
 export class ConfirmPaymentDto {
   @IsString()
@@ -6,4 +6,9 @@ export class ConfirmPaymentDto {
 
   @IsString()
   bookingId: string;
+
+  // Add optional field for user email (backend will set it, not required from frontend)
+  @IsOptional()
+  @IsString()
+  userEmail?: string;
 }
