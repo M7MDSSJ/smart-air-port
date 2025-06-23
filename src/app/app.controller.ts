b694c6e1b61fd756aa869143fe41a15cbc7b906f
sayed <terminalexe1@gmail.com>
2025-06-23 19:09:16 +0300

    i

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -8,7 +8,7 @@ export class HealthController {
   @ApiOperation({ summary: 'Check API health status' })
   healthCheck() {
     return {
-      status: 'fine1',
+      status: 'ismail1',
       timestamp: new Date().toISOString(),
       environment: process.env.NODE_ENV || 'development',
       version: process.env.npm_package_version || '0.0.1',
