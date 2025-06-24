d679a87e5b8a76121a3ae65598ead790390cd532
Ali Mohammed <terminalexe1@gmail.com>
2025-06-24 18:57:44 +0300

    test2

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -8,11 +8,7 @@ export class HealthController {
   @ApiOperation({ summary: 'Check API health status' })
   healthCheck() {
     return {
-      status: 'ismaiVM',
-      timestamp: new Date().toISOString(),
-      environment: process.env.NODE_ENV || 'development',
-      version: process.env.npm_package_version || '0.0.1',
-      uptime: process.uptime()
+     "VM2 TEST"
     };
   }
 }
